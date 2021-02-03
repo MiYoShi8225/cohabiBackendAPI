59d885875c207063332e397c980ff29221bd1004
MiYoShi8225 <down8225@gmail.com>
2021-02-03 23:45:19 +0900

    2021/2/3: todoの機能を追加

--- prod_env/DynamoDBAccessFunciton.py
+++ prod_env/DynamoDBAccessFunciton.py
@@ -7,33 +7,46 @@ import json
 
 dynamodb = boto3.resource('dynamodb')  # AWSサービスのリソースタイプの宣言
 table = dynamodb.Table("COHABI-USER-DATABASE")  # dynamoテーブル名
-#table = dynamodb.Table("accounting-book") #旧dynamoテーブル
+
 
 class Dynamo_Access:
     def __init__(self, request, route, body, group_id, data_id, userid, date_sort):
         self.exe_result = []
         if request == "POST":
             post_req = Post_Request(body, group_id, data_id, userid)
+
             if route == "/costs":
                 post_req.costs_post()
             if route == "/categories":
                 post_req.categories_post()
+            if route == "/todos":
+                post_req.todos_post()
+
         elif request == "GET":
             get_req = Get_Request(group_id, date_sort)
+
             if route == "/costs/{yyyy}/{MM}":
                 self.exe_result.append(get_req.costs_get())
             if route == "/categories":
                 self.exe_result.append(get_req.categories_get())
+            if route == "/todos":
+                self.exe_result.append(get_req.todos_get())
 
         elif request == "PUT":
             put_req = Put_Request(body, group_id, data_id, userid)
+
             if route == "/costs":
                 put_req.costs_put()
+            if route == "/todos":
+                put_req.todos_put()
+
         elif request == "DELETE":
             put_req = Del_Request(group_id, data_id)
+
             if route == "/costs":
                 put_req.costs_del()
-            
+            if route == "/todos":
+                put_req.todos_del()
 
     def return_result(self):
         return self.exe_result
@@ -46,14 +59,14 @@ class Post_Request:
         self.data_id = data_id
         self.user_id = userid
         self.date_now = datetime.datetime.now()
-        
 
     def costs_post(self):
         Value = self.body['value']
         Category = self.body['category']
         Comment = self.body['comment']
         GroupID = "COSTS_" + self.group_id
-        self.date = self.body['date'].split('/')[0] + self.body['date'].split('/')[1] + self.body['date'].split('/')[2]
+        self.date = self.body['date'].split(
+            '/')[0] + self.body['date'].split('/')[1] + self.body['date'].split('/')[2]
         Date = self.date + "_" + self.date_now.strftime('%H%M%S%f')
 
         putResponse = table.put_item(
@@ -88,10 +101,35 @@ class Post_Request:
 
         print(putResponse)
 
+    def todos_post(self):
+        Comment = self.body['comment']
+        GroupID = "TODOS_" + self.group_id
+        Value = self.body['name']
+
+        if self.body['done'] == True:
+            Status = "done"
+        else:
+            Status = "yet"
+
+        putResponse = table.put_item(
+            Item={
+                'ID': GroupID,
+                'DATA_TYPE': self.date_now.strftime('20%y%m%d_%H%M%S%f'),
+                'DATA_VALUE': Value,
+                'COMMENT': Comment,
+                'TIMESTAMP': self.date_now.strftime('20%y%m%d%H%M%S%f'),
+                'STATUS': Status
+            }
+        )
+
+        print(putResponse)
+
+
 class Get_Request:
     def __init__(self, group_id, date_sort):
         self.group_id = str(group_id)
         self.date_sort = date_sort
+        self.bodyItems = []
 
     def costs_get(self):
         GroupID = "COSTS_" + self.group_id
@@ -99,14 +137,12 @@ class Get_Request:
             KeyConditionExpression=Key("ID").eq(
                 GroupID) & Key("DATA_TYPE").begins_with(self.date_sort)
         )
-        
-        bodyItems=[]
 
         for f in dynamoData["Items"]:
             tmp = f["DATA_TYPE"].split('_')[0]
-            #tmp[:4]はスタート0配列目から4ステップまで、tmp[4:6]は4配列目から6ステップまで、tmp[6:]はエンドから6配列目まで
+            # tmp[:4]はスタート0配列目から4ステップまで、tmp[4:6]は4配列目から6ステップまで、tmp[6:]はエンドから6配列目まで
             date_reverce_proc = tmp[:4] + "/" + tmp[4:6] + "/" + tmp[6:]
-            bodyItems.append({
+            self.bodyItems.append({
                 "date": date_reverce_proc,
                 "id": f["DATA_TYPE"],
                 "value": f["DATA_VALUE"],
@@ -115,26 +151,46 @@ class Get_Request:
                 "comment": f["COMMENT"],
                 "timestamp": f["TIMESTAMP"]
             })
-        
-        return bodyItems
-    
+
+        return self.bodyItems
+
     def categories_get(self):
         GroupID = "CATEGORIES_" + self.group_id
         dynamoData = table.query(
             KeyConditionExpression=Key("ID").eq(GroupID) & Key("DATA_TYPE").begins_with("No"))
-        
-        bodyItems=[]
 
         for f in dynamoData["Items"]:
             #tmp = f["DATA_TYPE"].split('_')[0]
 
-            bodyItems.append({
+            self.bodyItems.append({
                 "id": f["DATA_TYPE"].split('_')[1],
                 "name": f["DATA_VALUE"],
                 "disabled": f["DISABLED"]
             })
-        
-        return bodyItems
+
+        return self.bodyItems
+
+    def todos_get(self):
+        GroupID = "TODOS_" + self.group_id
+        dynamoData = table.query(
+            KeyConditionExpression=Key("ID").eq(GroupID))
+
+        for f in dynamoData["Items"]:
+            #tmp = f["DATA_TYPE"].split('_')[0]
+
+            if f["STATUS"] == "done":
+                STATUS = True
+            else:
+                STATUS = False
+
+            self.bodyItems.append({
+                "id": f["DATA_TYPE"],
+                "name": f["DATA_VALUE"],
+                "comment": f["COMMENT"],
+                "done": STATUS
+            })
+
+        return self.bodyItems
 
 
 class Put_Request:
@@ -144,8 +200,7 @@ class Put_Request:
         self.group_id = str(group_id)
         self.data_id = data_id
         self.user_id = userid
-        self.date = self.body['date'].split('/')[0] + self.body['date'].split('/')[1] + self.body['date'].split('/')[2]
-    
+
     def costs_put(self):
         Value = self.body['value']
         Category = self.body['category']
@@ -166,6 +221,31 @@ class Put_Request:
         )
         print(putResponse)
 
+    def todos_put(self):
+        Comment = self.body['comment']
+        GroupID = "TODOS_" + self.group_id
+        Value = self.body['name']
+        Data = self.data_id
+
+        if self.body['done'] == True:
+            Status = "done"
+        else:
+            Status = "yet"
+
+        putResponse = table.put_item(
+            Item={
+                'ID': GroupID,
+                'DATA_TYPE': Data,
+                'DATA_VALUE': Value,
+                'COMMENT': Comment,
+                'TIMESTAMP': self.date_now.strftime('20%y%m%d%H%M%S%f'),
+                'STATUS': Status
+            }
+        )
+
+        print(putResponse)
+
+
 class Del_Request:
     def __init__(self, group_id, data_id):
         self.group_id = str(group_id)
@@ -182,4 +262,13 @@ class Del_Request:
         print("##delete dynamo data.")
         print(putResponse)
 
-
+    def todos_del(self):
+        GroupID = "TODOS_" + self.group_id
+        putResponse = table.delete_item(
+            Key={
+                'ID': GroupID,
+                'DATA_TYPE': self.data_id,
+            }
+        )
+        print("##delete dynamo data.")
+        print(putResponse)
